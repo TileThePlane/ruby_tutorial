9c8288e7d0f9819946c66d63b761b816462d661f
Kyle <kvonderw@uci.edu>
2018-10-15 23:42:19 -0700

    select example

--- triumvirate.rb
+++ triumvirate.rb
@@ -1,3 +1,5 @@
 ['Tedly', 'Brookhouse', 'Sanders', 'Elko', 'SLURM'].map(&:downcase)
 
 ['smol', 'small', 'gucci','sum  body'].map { |i| puts i.upcase.split.join('-') }
+
+[1,2,3,4,5,6,7,8,9].select { |n| puts(n.to_s ,n % 2 == 0) }
