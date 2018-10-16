ea1cbc380a9ece519d9156dbb513de6cf7beffdb
Kyle <kvonderw@uci.edu>
2018-10-16 13:17:23 -0700

    Add upcase method

--- palindrome.rb
+++ palindrome.rb
@@ -9,10 +9,15 @@ class Phrase
 		processed_content = self.content.downcase
 		processed_content == processed_content.reverse
 	end
+
+	def louder
+		self.content.upcase
+	end
 end
 
 phrase = Phrase.new("Racecar")
 puts phrase.content
 puts phrase.palindrome?
+puts phrase.louder
 
 
