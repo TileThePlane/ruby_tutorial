e182875bb45f6aa0a7a142b76c2d200630908ac6
Kyle <kvonderw@uci.edu>
2018-10-16 13:14:43 -0700

    Palindrome class

--- palindrome.rb
+++ palindrome.rb
@@ -1,4 +1,18 @@
-def palindrome?(str)
-	str.downcase == str.downcase.reverse
+class Phrase
+	attr_accessor :content
+
+	def initialize(content)
+		@content = content
+	end
+
+	def palindrome?
+		processed_content = self.content.downcase
+		processed_content == processed_content.reverse
+	end
 end
+
+phrase = Phrase.new("Racecar")
+puts phrase.content
+puts phrase.palindrome?
+
 
